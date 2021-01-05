66fa004031f27ae08c4b132107052d687f3d91a1
nguyenGALAXY <67467375+nguyenGALAXY@users.noreply.github.com>
2021-01-05 13:49:58 +0700

    create detail page

---
+++ routes/detail.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const auth = require("../middlewares/auth.mdw");
+
+const router = express.Router();
+
+router.get("/", async function (req, res) {
+    res.render("viewCourseDetail/detail", {
+      layout: "main.hbs",
+    });
+  });
+
+  module.exports = router;
