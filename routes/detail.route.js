4684f8bcee65a0af9eeda42bca36f0221dd6be34
nguyenGALAXY <67467375+nguyenGALAXY@users.noreply.github.com>
2021-01-06 12:14:58 +0700

    Update detailpage
    fix decription

--- routes/detail.route.js
+++ routes/detail.route.js
@@ -4,6 +4,7 @@ const auth = require("../middlewares/auth.mdw");
 const router = express.Router();
 
 router.get("/", async function (req, res) {
+ 
     res.render("viewCourseDetail/detail", {
       layout: "main.hbs",
     });
